5d749c136afc2ea8c9da48eaff2bf4d29b2f4a3f
Dekdark4 <zzkskl@gmail.com>
2026-07-24 14:23:34 +0400

    Научил программу слушать порт 11000.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Server is starting...");
+
+TcpListener listener = new TcpListener(IPAddress.Any, 11000);
+
+listener.Start();
+
+Console.WriteLine("Server is listening on port 11000...");
+
+Console.ReadLine();
