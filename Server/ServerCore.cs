79437cabfd65561b960e49a4972fc0e3822c7aea
Dekdark4 <zzkskl@gmail.com>
2026-07-24 15:27:32 +0400

    Добавил метод Start в ServerCore.

--- Server/ServerCore.cs
+++ Server/ServerCore.cs
@@ -6,9 +6,17 @@ namespace Server;
 public class ServerCore
 {
     private readonly TcpListener _listener;
+    private bool _isRunning;
 
     public ServerCore(int port)
     {
         _listener = new TcpListener(IPAddress.Any, port);
+    }
+
+    public void Start()
+    {
+        _listener.Start();
+        _isRunning = true;
+        Console.WriteLine("Server is started and waiting for client...");
     }
 }
