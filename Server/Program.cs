96421af19b765b61db1350765927c46616c2950e
Dekdark4 <zzkskl@gmail.com>
2026-07-24 14:27:04 +0400

    Научил программу ждать TcpClient.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 
+const int PORT = 11000;
+
+
 Console.WriteLine("Server is starting...");
 
-TcpListener listener = new TcpListener(IPAddress.Any, 11000);
 
+TcpListener listener = new TcpListener(IPAddress.Any, PORT);
 listener.Start();
 
-Console.WriteLine("Server is listening on port 11000...");
+Console.WriteLine("Server is listening on port {0}...", PORT);
+
+
+Console.WriteLine("Waiting for client...");
+
+TcpClient client = listener.AcceptTcpClient();
+
+Console.WriteLine("Client connected.");
+
 
 Console.ReadLine();
