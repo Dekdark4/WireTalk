c7ab84a32724172dc5d74b2185146768d94ab06d
Dekdark4 <zzkskl@gmail.com>
2026-07-24 14:38:32 +0400

    Создал отдельный элемент ServerCore.cs, где будет находиться вся основная логика сервера.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,23 +1,9 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using Server;
 
 const int PORT = 11000;
 
+ServerCore server = new(PORT);
 
-Console.WriteLine("Server is starting...");
-
-
-TcpListener listener = new TcpListener(IPAddress.Any, PORT);
-listener.Start();
-
-Console.WriteLine("Server is listening on port {0}...", PORT);
-
-
-Console.WriteLine("Waiting for client...");
-
-TcpClient client = listener.AcceptTcpClient();
-
-Console.WriteLine("Client connected.");
-
+Console.WriteLine("Server is running on port " + PORT);
 
 Console.ReadLine();
---
+++ Server/ServerCore.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server;
+
+public class ServerCore
+{
+    private readonly TcpListener _listener;
+
+    public ServerCore(int port)
+    {
+        _listener = new TcpListener(IPAddress.Any, port);
+    }
+}
