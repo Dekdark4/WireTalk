65aea00e34b6552881b1aa3b1de2b889f38ee517
Dekdark4 <zzkskl@gmail.com>
2026-07-25 09:04:45 +0400

    Добавил бесконечный цикл для отправки сообщений в Program.cs.

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 
 const string SERVER_IP = "127.0.0.1";
 const int SERVER_PORT = 11000;
+const string EXIT_COMMAND = "/exit";
 
 Console.WriteLine("Client is starting...");
 
@@ -13,14 +14,33 @@ Console.WriteLine("Connecting to server...");
 await client.ConnectAsync(SERVER_IP, SERVER_PORT);
 
 Console.WriteLine("Connected to server.");
+Console.WriteLine($"Type {EXIT_COMMAND} to disconnect.");
 
 NetworkStream stream = client.GetStream();
-Console.Write("Message to send: ");
-string message = Console.ReadLine() ?? string.Empty;
 
-byte[] data = Encoding.UTF8.GetBytes(message);
-await stream.WriteAsync(data);
-Console.WriteLine("Message sent.");
+while(true)
+{
+    Console.Write("Message to send: ");
+    string message = Console.ReadLine() ?? string.Empty;
+
+    if(message == EXIT_COMMAND)
+    {
+        break;
+    }
+
+    if(message.Length == 0)
+    {
+        Console.WriteLine("Message cannot be empty.");
+        continue;
+    }
+
+    byte[] data = Encoding.UTF8.GetBytes(message);
+
+    await stream.WriteAsync(data);
+
+    Console.WriteLine("Message sent.");
+}
+
 
 Console.WriteLine("Press enter to disconnect...");
 Console.ReadLine();
