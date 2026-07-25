ec28dc87fc8e73b1ac29ef65abd1daf74ee8b88b
Dekdark4 <zzkskl@gmail.com>
2026-07-25 08:40:15 +0400

    Добавил подключение к серверу для клиента в Program.cs.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+
+const string SERVER_IP = "127.0.0.1";
+const int SERVER_PORT = 11000;
+
+Console.WriteLine("Client is starting...");
+
+using TcpClient client = new();
+
+Console.WriteLine("Connecting to server...");
+
+await client.ConnectAsync(SERVER_IP, SERVER_PORT);
+
+Console.WriteLine("Connected to server.");
+
+Console.WriteLine("Press enter to disconnect...");
+Console.ReadLine();
